544a21904a85fb6dd1e5cefcbaf5e799319d6520
edwardkurita <edwardcskurtia@gmail.com>
2024-09-24 16:17:20 -0400

    modified pseudocode, added empty txt files for input/output

--- main.c
+++ main.c
@@ -8,12 +8,25 @@ int main(void) {
     //pseudocode commenting:
 
     //initialize variables
+        //file for reading from the vector table
+        //file for reading the input file
+        //file for writing the output to
+        //int mode variable
+        //variable for the loop instance
+
+    //set up a 2D string array for each file
+        //file 1
+        //file 2
+
+    //set up the third file for writing
 
     //while loop for repeating the code until EOF
 
         //read_from_sensor() aka reading from the input txt file, I guess we'll do it so it reads one line of the file per loop?
+            //this needs to be able to read all the lines of the code at once, from CPU to END_IO, so maybe hard code it to read return 3 lines of the string array?
 
         //calculate the value that needs to be displayed from y
+            //input of Sting array,
 
         //display the value
 
@@ -23,5 +36,7 @@ int main(void) {
 }
 
 //function for file reading
+    //local variables:
+        //3d string array (using code block, line, part of line, sectioning).
 
 //function for value display
