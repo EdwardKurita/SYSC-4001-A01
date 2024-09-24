1eed979b4b8720b2c2feb4e43e60a00515391e43
edwardkurita <edwardcskurtia@gmail.com>
2024-09-24 15:37:10 -0400

    added pseudocode skeleton for the .exe

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+//function for file reading
+
+//function for value display
+
 int main(void) {
-    printf("Hello, World!\n");
+    //pseudocode commenting:
+
+    //initialize variables
+
+    //while loop for repeating the code until EOF
+
+        //read_from_sensor() aka reading from the input txt file, I guess we'll do it so it reads one line of the file per loop?
+
+        //calculate the value that needs to be displayed from y
+
+        //display the value
+
+        //loop to EOF
+
     return 0;
 }
+
+//function for file reading
+
+//function for value display
